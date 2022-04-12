d7aab9450d182eca9b6c431b84c3ceb7a6cd32e5
YECHANESS <qaz2722@naver.com>
2022-04-12 18:10:16 +0900

    랜덤맵 프리펩 꼬임 수정 및 리스폰 수정

--- Assets/Scripts/STAGE_MANAGEMENT/PlayerSetStartPostion.cs
+++ Assets/Scripts/STAGE_MANAGEMENT/PlayerSetStartPostion.cs
@@ -8,20 +8,24 @@ namespace STAGE_MANAGEMENT
     public class PlayerSetStartPostion : MonoBehaviour
     {
         //[Header("스폰시킬 플레이어")]
-        private GameObject Player;
+        private GameObject player;
         
         private void OnEnable()
         {
+            Respawn();
+        }
+
+        public void Respawn()
+        {
             // 현재 Scene에 플레이어가 없는지 확인
-            if (GameObject.FindGameObjectWithTag("Player") == null) 
+            if (GameObject.FindGameObjectWithTag("Player") != null)
             {
-                // 플레이어 생성
-                Instantiate(Player, this.transform);
-            }
-            else
-            {
+                player = GameObject.FindGameObjectWithTag("Player");
                 // 플레이어 시작 지점 스폰
-                GameObject.FindGameObjectWithTag("Player").transform.position = this.transform.position;
+                
+                player.transform.position = this.transform.position;
+                //player.GetComponent<CharacterController>().Move(this.transform.position);
+                Debug.Log($"플레이어 : {player.transform.position} / 스폰 : {this.transform.position}");
             }
         }
     }
