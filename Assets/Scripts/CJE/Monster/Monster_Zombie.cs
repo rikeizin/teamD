abbd662d1ce7fd39f602270e44bb1c946f0e094a
choije01 <102936391+choije01@users.noreply.github.com>
2022-06-13 09:30:48 +0900

    2022.06.13 HP조정

--- Assets/Scripts/CJE/Monster/Monster_Zombie.cs
+++ Assets/Scripts/CJE/Monster/Monster_Zombie.cs
@@ -9,7 +9,15 @@ public class Monster_Zombie : MonsterController
     protected override void OnAwake()
     {        
         base.OnAwake();
+<<<<<<< Updated upstream
         m_status = new Status(40f, 5f, 1f, 1f, 50f); //(int hp, float attack, float attackRange, float hitRange, float trackingRange)
+=======
+<<<<<<< HEAD
+        m_status = new Status(100f, 5f, 0.7f, 1f, 50f); //(int hp, float attack, float attackRange, float hitRange, float trackingRange)
+=======
+        m_status = new Status(40f, 5f, 1f, 1f, 50f); //(int hp, float attack, float attackRange, float hitRange, float trackingRange)
+>>>>>>> 91f5d3b0ebd7d2a7eed9307fbf9045051288bfdf
+>>>>>>> Stashed changes
         m_Hpbar.value = m_status.m_hp / m_status.m_hpMax * 100;
     }
 
