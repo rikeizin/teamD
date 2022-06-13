06bfbf15f22b8fe75a60b32631be2320c3491491
LimMJ <lunar920717@gmail.com>
2022-06-13 11:07:59 +0900

    Monster
    스텟 수정

--- Assets/Scripts/CJE/Monster/Monster_TurtleShell.cs
+++ Assets/Scripts/CJE/Monster/Monster_TurtleShell.cs
@@ -11,7 +11,7 @@ public class Monster_TurtleShell : MonsterController
     protected override void OnAwake()
     {
         base.OnAwake();
-        m_status = new Status(30f, 5f, 1f, 1f, 50f); //(int hp, float attack, float attackRange, float hitRange, float trackingRange)
+        m_status = new Status(100f, 5f, 1f, 1f, 50f); //(int hp, float attack, float attackRange, float hitRange, float trackingRange)
         m_Hpbar.value = m_status.m_hp / m_status.m_hpMax * 100;
     }
 
